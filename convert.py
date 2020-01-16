196a755994d7022b8c42eeee6754c7073f86c192
Koshi1989 <kmuraokshi@hks.harvard.edu>
2020-01-16 14:24:25 -0500

    add data conversion example7

--- convert.py
+++ convert.py
@@ -1,15 +1,34 @@
-#!/usr/bin/env python3
+#Ahmed, Hana and Koshi Section b
 
+#!/usr/bin/env python3
 import csv
 import datetime
 
 # 1. load input.csv into a variable called `polls`
+with open('input.csv', 'r') as f:
+	reader = csv.DictReader(f)
+	rows = list(reader)
+	polls = [dict(row) for row in rows]
 
 # 2. write a new file called output.csv and write a row with two headers: "date" and "approve"
+with open('output.csv', 'w') as f:
+	writer = csv.writer(f)
+	writer.writerow(['date','approve'])
 
 # 3. Loop through each row of `polls` 
+	for poll in polls:
+		enddate = poll['enddate']
+		approve = poll['approve']
 
     # 4. and within that loop... convert the format of `enddate` from "1/22/2017" to "22-Jan-17"
+		pydate = datetime.datetime.strptime(enddate, "%m/%d/%Y")
+		new_date = pydate.strftime("%d-%b-%y")
+    
+    # 5. write a new row of data with the transformed date and value for "approve" 
+
+		writer.writerow([new_date, approve])
+
+
     # hint: to read the date you will need to use
     #       date = datetime.datetime.strptime(myrawstring, "insert input format here")
     #
@@ -18,4 +37,3 @@ import datetime
     # 
     #       date formats can be found at https://strftime.org/
     
-    # 5. write a new row of data with the transformed date and value for "approve" 
